f4063170b148a5d38cba978df88c80cf09ee9a8e
retrotshepo <tmabine@retrorabbit.co.za>
2021-02-05 09:50:57 +0200

    data service interface with connection

--- dvt/app/src/main/java/za/co/weather/weather_app/util/WeatherAPIDataService.kt
+++ dvt/app/src/main/java/za/co/weather/weather_app/util/WeatherAPIDataService.kt
@@ -4,10 +4,13 @@ import org.json.JSONObject
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
+import za.co.weather.weather_app.util.CallValues.Companion.UNITS
+import za.co.weather.weather_app.util.RetrofitService.Companion.BASE_URL_ROAD_WARRIOR
 
 interface WeatherAPIDataService {
 
-    @GET("")
-    suspend fun getWeatherDataAPI(): JSONObject
+    @GET("data/2.5/forecast?appid=230d8042e9393c5f5644efb4b4bcdf5e&units=metric&mode=json&cnt=40")
+    suspend fun getWeatherDataAPI(@Query("lat") lat: Double, @Query("lon") lon: Double): Any
 
 }
