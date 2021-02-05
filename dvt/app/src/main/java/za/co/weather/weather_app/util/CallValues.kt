da3847383b246ed63be1bdfd36c48cdd8d91c6cc
retrotshepo <tmabine@retrorabbit.co.za>
2021-02-05 09:45:18 +0200

    updated build gradle; call query parameters

--- dvt/app/src/main/java/za/co/weather/weather_app/util/CallValues.kt
+++ dvt/app/src/main/java/za/co/weather/weather_app/util/CallValues.kt
@@ -1,4 +1,10 @@
 package za.co.weather.weather_app.util
 
 class CallValues {
+
+    companion object {
+        var LATITUDE = 0L
+        var LONGITUDE = 0L
+        var UNITS = "metric"
+    }
 }
