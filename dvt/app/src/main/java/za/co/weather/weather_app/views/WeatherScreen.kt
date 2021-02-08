8cdacefb756057b7fa1578ecc879202f085b5c2f
retrotshepo <tmabine@retrorabbit.co.za>
2021-02-08 10:37:04 +0200

    weather screen init

--- dvt/app/src/main/java/za/co/weather/weather_app/views/WeatherScreen.kt
+++ dvt/app/src/main/java/za/co/weather/weather_app/views/WeatherScreen.kt
@@ -1,4 +1,23 @@
 package za.co.weather.weather_app.views
 
-class WeatherScreen {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import za.co.weather.weather_app.R
+
+class WeatherScreen : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.layout_main_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        println("WeatherScreen onViewCreated")
+    }
 }
