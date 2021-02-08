f8b35d45b4598d52c505e67ffb8242e3d72d82f7
retrotshepo <tmabine@retrorabbit.co.za>
2021-02-08 14:38:30 +0200

    cleanup main activity

--- dvt/app/src/main/java/za/co/weather/weather_app/MainActivity.kt
+++ dvt/app/src/main/java/za/co/weather/weather_app/MainActivity.kt
@@ -2,16 +2,7 @@ package za.co.weather.weather_app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.google.gson.JsonObject
-import com.google.gson.internal.GsonBuildConfig
-import kotlinx.android.synthetic.main.content_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import retrofit2.converter.gson.GsonConverterFactory
-import za.co.weather.weather_app.util.WeatherAPIEndpoint.Companion.getWeatherCALL
+import za.co.weather.weather_app.util.SideNavigationOptions.Companion.homeScreen
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,30 +10,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_test.setOnClickListener {
-            println("hello world!")
-
-            this@MainActivity.lifecycleScope.launch (Dispatchers.IO) {
-
-//                [-26.5603, 27.8625]
-                val response = getWeatherCALL(-26.5603,27.8625)
-
-//                JSONObject
-
-                if (response != null) {
-
-//                    val gg = JSONObject(response)
-//                    gson.fromJson(theJson, JsonObject::class.java)
-
-                    val gson = Gson()
-                    val json = gson.toJsonTree(response).asJsonObject
-
-
-                    println(json.javaClass)
-                    println(json)
-
-                }
-            }
-        }
+        homeScreen(this)
     }
+
+
 }
