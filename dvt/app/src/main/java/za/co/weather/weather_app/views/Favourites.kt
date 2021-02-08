55791457e3ac26f2a30b67736785cceeda25d68b
retrotshepo <tmabine@retrorabbit.co.za>
2021-02-08 14:42:24 +0200

    fav fragment - update main screen layout

--- dvt/app/src/main/java/za/co/weather/weather_app/views/Favourites.kt
+++ dvt/app/src/main/java/za/co/weather/weather_app/views/Favourites.kt
@@ -1,4 +1,27 @@
 package za.co.weather.weather_app.views
 
-class Favourites {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Favourites : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+
 }
