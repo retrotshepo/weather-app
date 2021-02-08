60f722ea56ed5cd2bac8c66d3c67cf7c93a26c64
retrotshepo <tmabine@retrorabbit.co.za>
2021-02-08 14:41:34 +0200

    added side nav options

--- dvt/app/src/main/java/za/co/weather/weather_app/util/SideNavigationOptions.kt
+++ dvt/app/src/main/java/za/co/weather/weather_app/util/SideNavigationOptions.kt
@@ -1,4 +1,30 @@
 package za.co.weather.weather_app.util
 
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import za.co.weather.weather_app.R
+import za.co.weather.weather_app.views.Favourites
+import za.co.weather.weather_app.views.WeatherScreen
+
 class SideNavigationOptions {
+
+    companion object {
+
+        fun homeScreen(context: AppCompatActivity) {
+            loadFragment(context, WeatherScreen())
+        }
+
+        fun favouritesScreen(context: AppCompatActivity) {
+            loadFragment(context, Favourites())
+
+        }
+
+        private fun loadFragment(activity: AppCompatActivity, fragment: Fragment, holder: Int = R.id.main_content_frame) {
+            val transaction = activity.supportFragmentManager.beginTransaction()
+            transaction.addToBackStack(fragment.javaClass.simpleName)
+
+            transaction.replace(holder, fragment)
+            transaction.commit()
+        }
+    }
 }
