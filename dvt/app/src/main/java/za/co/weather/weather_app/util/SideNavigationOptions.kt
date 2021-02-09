6f4296e7e24265c9ca0130f436236bb91cfd017e
retrotshepo <tmabine@retrorabbit.co.za>
2021-02-09 22:42:02 +0200

    navigation helper done

--- dvt/app/src/main/java/za/co/weather/weather_app/util/SideNavigationOptions.kt
+++ dvt/app/src/main/java/za/co/weather/weather_app/util/SideNavigationOptions.kt
@@ -3,7 +3,9 @@ package za.co.weather.weather_app.util
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import za.co.weather.weather_app.R
+import za.co.weather.weather_app.views.FavouriteCity
 import za.co.weather.weather_app.views.Favourites
+import za.co.weather.weather_app.views.SearchCity
 import za.co.weather.weather_app.views.WeatherScreen
 
 class SideNavigationOptions {
@@ -16,7 +18,14 @@ class SideNavigationOptions {
 
         fun favouritesScreen(context: AppCompatActivity) {
             loadFragment(context, Favourites())
+        }
 
+        fun cityScreen(context: AppCompatActivity) {
+            loadFragment(context, FavouriteCity())
+        }
+
+        fun searchScreen(context: AppCompatActivity) {
+            loadFragment(context, SearchCity())
         }
 
         private fun loadFragment(activity: AppCompatActivity, fragment: Fragment, holder: Int = R.id.main_content_frame) {
