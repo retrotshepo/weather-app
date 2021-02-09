7ef00a625ad54513a751a5f8c003e83cbcad8e26
retrotshepo <tmabine@retrorabbit.co.za>
2021-02-09 09:58:17 +0200

    added more current temp details

--- dvt/app/src/main/java/za/co/weather/weather_app/views/WeatherScreen.kt
+++ dvt/app/src/main/java/za/co/weather/weather_app/views/WeatherScreen.kt
@@ -5,6 +5,7 @@ import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,12 +14,15 @@ import com.google.gson.Gson
 import com.google.gson.JsonObject
 import com.google.gson.internal.LinkedTreeMap
 import kotlinx.android.synthetic.main.layout_weather_screen.*
+import kotlinx.android.synthetic.main.navigation_view_header.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.json.JSONObject
 import za.co.weather.weather_app.R
 import za.co.weather.weather_app.util.WeatherAPIEndpoint
+import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.roundToInt
 
 class WeatherScreen : Fragment() {
 
@@ -57,7 +61,10 @@ class WeatherScreen : Fragment() {
 
         this@WeatherScreen.lifecycleScope.launch (Dispatchers.IO) {
 
-//                [-26.5603, 27.8625]
+//                [-26.5603, 27.8625] orange farm
+//            -26.1846304,28.0014622 ACK
+//            -33.9152208,18.3758763 CPT
+//            -30.7158878,30.3887406 NPS
             val resCurrent = WeatherAPIEndpoint.getWeatherCurrentCALL(-26.5603, 27.8625)
             val resForecast = WeatherAPIEndpoint.getWeatherForecastCALL(-26.5603, 27.8625)
 
@@ -66,17 +73,21 @@ class WeatherScreen : Fragment() {
                 val res = serializeToJSONObject(resCurrent as LinkedTreeMap<Any, Any>)
 
                 if (res != null) {
-                     current = CurrentTemperatureData(
+                    current = CurrentTemperatureData(
                         res.getJSONObject("coord"), res.getJSONArray("weather"),
                         res.getString("base"), res.getJSONObject("main"),
                         res.getDouble("visibility"), res.getJSONObject("wind"),
                         res.getJSONObject("clouds"), "${res.getDouble("dt")}",
                         res.getJSONObject("sys"), res.getLong("timezone"),
                         "${res.getLong("id")}", res.getString("name"),
-                        "${res.getInt("cod")}")
+                        "${res.getInt("cod")}"
+                    )
 
                 }
 
+
+                println(" sunrise ${convertLongToTime(current?.sys?.getLong("sunrise"))}")
+                println(" sunset ${convertLongToTime(current?.sys?.getLong("sunset"))}")
                 println(res)
 
             }
@@ -84,7 +95,7 @@ class WeatherScreen : Fragment() {
             if (resForecast != null) {
 
                 val res = serializeToJSONObject(resForecast as LinkedTreeMap<Any, Any>)
-
+forecast = arrayListOf()
                 if (res != null) {
                     val list = res.getJSONArray("list")
                     for(i in 0 until list.length()) {
@@ -126,13 +137,32 @@ class WeatherScreen : Fragment() {
             return
         }
 
-        temp_number_main.text = "${currentTemperatureData.main.getDouble("temp")}"
+        temp_number_main.text = "${currentTemperatureData.main.getDouble("temp").roundToInt()}\u00B0"
         temp_condition_main.text = currentTemperatureData.weather.getJSONObject(0)
             .getString("main").toUpperCase(Locale.ENGLISH)
 
-        temp_min_text.text = "${currentTemperatureData.main.getDouble("temp_min")}\nmin"
-        temp_curr_text.text = "${currentTemperatureData.main.getDouble("feels_like")}\nFeels like"
-        temp_max_text.text = "${currentTemperatureData.main.getDouble("temp_max")}\nmax"
+        temp_min_text.text = "${currentTemperatureData.main.getDouble("temp_min")}\u00B0\nmin"
+        temp_curr_text.text = "${currentTemperatureData.main.getDouble("temp")}\u00B0\nCurrent"
+        temp_max_text.text = "${currentTemperatureData.main.getDouble("temp_max")}\u00B0\nmax"
+
+        curr_location.text = "${currentTemperatureData.name}, ${currentTemperatureData.sys.getString("country")}"
+        curr_humidity.text = "HUMIDITY\n${currentTemperatureData.main.getDouble("humidity").roundToInt()}%"
+        curr_pressure.text = "PRESSURE\n${currentTemperatureData.main.getDouble("pressure").roundToInt()}hPa"
+//        location_text.text = "hello world"
+
+        if(currentTemperatureData.weather.getJSONObject(0).getString("main").contains("cloud", true)) {
+
+            background_main.background = ActivityCompat.getDrawable(requireContext(), R.drawable.forest_cloudy)
+            main_layout.background = ActivityCompat.getDrawable(requireContext(), R.drawable.main_background_cloud)
+        }
+        else if(currentTemperatureData.weather.getJSONObject(0).getString("main").contains("clear", true)) {
+            background_main.background = ActivityCompat.getDrawable(requireContext(), R.drawable.forest_sunny)
+            main_layout.background = ActivityCompat.getDrawable(requireContext(), R.drawable.main_background_clear)
+        }
+        else if(currentTemperatureData.weather.getJSONObject(0).getString("main").contains("rain", true)) {
+            background_main.background = ActivityCompat.getDrawable(requireContext(), R.drawable.forest_rainy)
+            main_layout.background = ActivityCompat.getDrawable(requireContext(), R.drawable.main_background_rain)
+        }
 
         /**
          * setting up adapter.
@@ -152,5 +182,16 @@ class WeatherScreen : Fragment() {
         println(json)
 
         return JSONObject(json.toString())
+    }
+
+    fun convertLongToTime(time: Long?): String {
+        if (time != null) {
+            if (time > 0) {
+                val date = Date(time)
+                val format = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+                return format.format(date)
+            }
+        }
+        return ""
     }
 }
