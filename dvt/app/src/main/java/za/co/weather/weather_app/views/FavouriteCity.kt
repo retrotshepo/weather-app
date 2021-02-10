825c978a34bc59e40da5cc8c11dcf8343bae7ec1
retrotshepo <tmabine@retrorabbit.co.za>
2021-02-10 12:49:25 +0200

    app ready? keys!!

--- dvt/app/src/main/java/za/co/weather/weather_app/views/FavouriteCity.kt
+++ dvt/app/src/main/java/za/co/weather/weather_app/views/FavouriteCity.kt
@@ -8,17 +8,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.layout_weather_screen.*
 import kotlinx.coroutines.runBlocking
 import za.co.weather.weather_app.R
 import za.co.weather.weather_app.model.CurrentTemperatureData
 import za.co.weather.weather_app.model.DailyTemperatureData
 import za.co.weather.weather_app.util.LocalDBHandler.Companion.update
+import za.co.weather.weather_app.util.NavigationRoutes
 import za.co.weather.weather_app.util.Util
 import za.co.weather.weather_app.util.Util.Companion.cityName
 import za.co.weather.weather_app.util.Util.Companion.makeApiCall
@@ -36,10 +40,18 @@ class FavouriteCity : Fragment() {
         return inflater.inflate(R.layout.layout_weather_screen, container, false)
     }
 
-    //                [-26.5603, 27.8625] orange farm
-//            -26.1846304,28.0014622 ACK
-//            -33.9152208,18.3758763 CPT
-//            -30.7158878,30.3887406 NPS
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_refresh.setOnClickListener {
+            NavigationRoutes.reloadCurrentFragment(requireActivity() as AppCompatActivity, this)
+        }
+
+        burger_menu.setOnClickListener {
+            activity?.weather_drawer_layout?.openDrawer(GravityCompat.START)
+        }
+    }
+
     override fun onResume() = runBlocking {
         super.onResume()
 
--- dvt/app/src/main/java/za/co/weather/weather_app/views/WeatherScreen.kt
+++ dvt/app/src/main/java/za/co/weather/weather_app/views/WeatherScreen.kt
@@ -7,17 +7,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.layout_weather_screen.*
 import kotlinx.coroutines.runBlocking
 import za.co.weather.weather_app.R
 import za.co.weather.weather_app.model.CurrentTemperatureData
 import za.co.weather.weather_app.model.DailyTemperatureData
 import za.co.weather.weather_app.util.CustomLocationListener
+import za.co.weather.weather_app.util.NavigationRoutes
+import za.co.weather.weather_app.util.NavigationRoutes.Companion.reloadCurrentFragment
 import za.co.weather.weather_app.util.Util.Companion.isMobileDataConnected
 import za.co.weather.weather_app.util.Util.Companion.isWiFiConnected
 import za.co.weather.weather_app.util.Util.Companion.makeApiCall
@@ -39,6 +44,18 @@ class WeatherScreen : Fragment() {
         gps?.getLastKnownLocationGPS(requireContext())
         gps?.getLastKnownLocationNetwork(requireContext())
         return inflater.inflate(R.layout.layout_weather_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_refresh.setOnClickListener {
+            reloadCurrentFragment(requireActivity() as AppCompatActivity, this)
+        }
+
+        burger_menu.setOnClickListener {
+            activity?.weather_drawer_layout?.openDrawer(GravityCompat.START)
+        }
     }
 
     override fun onResume() = runBlocking {
