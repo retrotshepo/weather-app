e11b54c171f907dc4d2d20bd89b0fa6203037af2
retrotshepo <tmabine@retrorabbit.co.za>
2021-02-10 19:10:35 +0200

    trying to save blank city check

--- dvt/app/src/main/java/za/co/weather/weather_app/views/SearchCity.kt
+++ dvt/app/src/main/java/za/co/weather/weather_app/views/SearchCity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -90,7 +91,6 @@ class SearchCity : Fragment() {
                                     .getColor(requireActivity(), R.color.red)
                             )
                             txt_search_error.text = e.message
-
                         }
                     }
                 }
@@ -98,8 +98,17 @@ class SearchCity : Fragment() {
         }
 
         btn_save.setOnClickListener {
-            create(current)
-            activity?.onBackPressed()
+
+            if (current == null) {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.search_cannot_save),
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                create(current)
+                activity?.onBackPressed()
+            }
         }
     }
 }
