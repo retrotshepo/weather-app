6807ca2339758dbadd3e1d7ee501211ec24f2d85
retrotshepo <tmabine@retrorabbit.co.za>
2021-02-12 18:27:48 +0200

    forecaset obj added

--- dvt/app/src/main/java/za/co/weather/weather_app/model/ForecastObject.kt
+++ dvt/app/src/main/java/za/co/weather/weather_app/model/ForecastObject.kt
@@ -1,4 +1,19 @@
 package za.co.weather.weather_app.model
 
-class ForecastObject {
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.RealmClass
+
+@RealmClass
+open class ForecastObject: RealmObject() {
+
+    @PrimaryKey
+    var id = 0L
+    var name: String = ""
+
+    var main: String = ""
+    var weather: String = ""
+
+    var date: String = ""
+    var city: String = ""
 }
