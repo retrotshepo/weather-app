e9770776d16a6b9303c1cc4cde3e62ed0867b5a1
retrotshepo <tmabine@retrorabbit.co.za>
2021-02-15 11:38:23 +0200

    added points to map

--- dvt/app/src/main/java/za/co/weather/weather_app/util/CustomMapsHandler.kt
+++ dvt/app/src/main/java/za/co/weather/weather_app/util/CustomMapsHandler.kt
@@ -7,8 +7,11 @@ import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import za.co.weather.weather_app.util.LocalDBHandler.Companion.readFavourites
+import kotlin.math.roundToInt
 
 class CustomMapsHandler(var context: Context) : OnMapReadyCallback {
 
@@ -16,21 +19,20 @@ class CustomMapsHandler(var context: Context) : OnMapReadyCallback {
 
     override fun onMapReady(p0: GoogleMap?) {
         mGoogleMap = p0
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-26.1942764, 28.0049734)
-        mGoogleMap?.addMarker(
-            MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney")
-        )
-        mGoogleMap?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
+        readFavourites().forEach {item ->
+            mGoogleMap?.addMarker(
+                MarkerOptions()
+                    .position(LatLng(item.coordinates.getDouble("lat"),item.coordinates.getDouble("lon")))
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                    .title("${item.name}, ${item.sys.getString("country")}")
+                    .snippet("Expect ${item.weather.getJSONObject(0)
+                        .getString("description")}, with temperatures peaking at ${item.main.getDouble("temp_max").roundToInt()}\u00B0")
+            )
+        }
 
         if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
             == PackageManager.PERMISSION_GRANTED) {
             mGoogleMap?.isMyLocationEnabled = true
         }
     }
-
 }
